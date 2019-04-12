375de70f8f94ecdfde2b83fed2118abb49e12d8c
Jessica Walker <peskywabbit@hotmail.com>
2019-04-12 14:34:40 -0700

    tcl

--- src/components/ToDo.js
+++ src/components/ToDo.js
@@ -1,16 +1,16 @@
 import React, { Component } from 'react';
 
-/* jshint ignore:start */
 class ToDo extends Component {
   render() {
     return (
       <li>
        <input type="checkbox" checked={ this.props.isCompleted } onChange={ this.props.toggleComplete } />
+
        <span>{ this.props.description }</span>
+       <button>DELETE<button>
       </li>
     );
   }
 }
-/* jshint ignore:end */
 
 export default ToDo;
